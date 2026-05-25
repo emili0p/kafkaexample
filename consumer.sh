6e7575df2a901e5d6e6bcf3f3764ba597412fa65
emili0p <emiliioizq23@gmail.com>
2026-05-24 23:08:35 -0600

    primer commit

---
+++ consumer.sh
@@ -0,0 +1,4 @@
+kafka-console-consumer.sh \
+  --topic detecciones \
+  --from-beginning \
+  --bootstrap-server localhost:9092
---
+++ topic.sh
@@ -0,0 +1,4 @@
+kafka-topics.sh \
+  --create \
+  --topic detecciones \
+  --bootstrap-server localhost:9092
